adb12be704bb95b4099908a66d6eb17cd98c14a2
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-10-03 21:47:32 -0300

    chore done: button created with 3 or more characters - task 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -11,16 +11,17 @@ import Search from './pages/Search';
 class App extends React.Component {
   render() {
     return (
-      <>
-        <p>TrybeTunes!</p>
-        <Route path="/" component={ Login } />
-        <Route path="/search" component={ Search } />
-        <Route path="/album/:id" component={ Album } />
-        <Route path="/favorites" component={ Favorites } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="*" component={ NotFound } />
-      </>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,54 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loginName: '',
+      minChars: true,
+    };
+  }
+
+  handleDisableBtn = () => {
+    const { loginName } = this.state;
+    const min = 3;
+    const minLength = loginName.length >= min;
+
+    this.setState({ minChars: !minLength });
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    console.log(value);
+    this.setState({
+      [name]: value,
+    }, () => this.handleDisableBtn());
+  };
+
   render() {
+    const {
+      loginName, minChars,
+    } = this.state;
     return (
       <div data-testid="page-login">
-        Login:
+        <p>TrybeTunes!</p>
+        <label htmlFor="login">
+          Login:
+          <input
+            type="text"
+            id="login"
+            name="loginName"
+            testid="login-name-input"
+            value={ loginName }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          disabled={ minChars }
+        >
+          Entrar
+        </button>
       </div>
     );
   }

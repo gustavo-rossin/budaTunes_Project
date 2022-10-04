91229fec7f1cbb70f62600944f8e40ddba2ae5f6
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-10-04 16:13:38 -0300

    task 5 done

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,54 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchBar: '',
+      minCharSearch: true,
+    };
+  }
+
+  disableSearchBtn = () => {
+    const { searchBar } = this.state;
+    const min = 2;
+    const minLengthSearch = searchBar.length >= min;
+
+    this.setState({ minCharSearch: !minLengthSearch });
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    }, () => this.disableSearchBtn());
+  };
+
   render() {
+    const { searchBar, minCharSearch } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form>
+          <label htmlFor="search">
+            <input
+              type="text"
+              id="search"
+              name="searchBar"
+              data-testid="search-artist-input"
+              value={ searchBar }
+              onChange={ this.handleChange }
+              placeholder="artistas que você ama"
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ minCharSearch }
+            // onClick={ this.handleUser }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }

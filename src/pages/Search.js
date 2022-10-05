4dbff7552ff307167dd6d0af17399c869de781b9
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-10-04 22:19:09 -0300

    task 6 done

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       searchBar: '',
+      searchBar2: '',
       minCharSearch: true,
+      loading: false,
+      albumInfo: [],
     };
   }
 
@@ -24,11 +30,24 @@ class Search extends React.Component {
     }, () => this.disableSearchBtn());
   };
 
+  handleAlbumAPI = async () => {
+    const { searchBar } = this.state;
+    // console.log(await searchAlbumsAPI({ name: 'hhhhh' }));
+    this.setState({ loading: true });
+    const api = await searchAlbumsAPI(searchBar);
+    this.setState({ loading: false,
+      albumInfo: api,
+      searchBar2: searchBar,
+      searchBar: '',
+    });
+  };
+
   render() {
-    const { searchBar, minCharSearch } = this.state;
+    const { searchBar, minCharSearch, loading, albumInfo, searchBar2 } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        { loading && <Loading /> }
         <form>
           <label htmlFor="search">
             <input
@@ -45,10 +64,36 @@ class Search extends React.Component {
             type="button"
             data-testid="search-artist-button"
             disabled={ minCharSearch }
-            // onClick={ this.handleUser }
+            onClick={ this.handleAlbumAPI }
           >
             Pesquisar
           </button>
+          {
+            albumInfo.length === 0
+              ? <h2>Nenhum álbum foi encontrado</h2>
+              : (
+                <div>
+                  <h2>
+                    Resultado de álbuns de:
+                    {' '}
+                    { searchBar2 }
+                  </h2>
+                  {
+                    albumInfo.map((item) => (
+                      <div key={ item.collectionId }>
+                        <img src={ item.artworkUrl100 } alt={ item.collectionName } />
+                        <h2>{item.collectionName}</h2>
+                        <h4>{item.artistName}</h4>
+                        <Link
+                          to={ `/album/${item.collectionId}` }
+                          data-testid={ `link-to-album-${item.collectionId}` }
+                        />
+                      </div>
+                    ))
+                  }
+                </div>
+              )
+          }
         </form>
       </div>
     );

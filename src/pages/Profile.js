2bcb97e8a743ea892b353291df59dc449fe59e30
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-10-10 11:48:03 -0300

    task 13 almost done, 1 error to fix

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      user: {},
+    };
+  }
+
+  async componentDidMount() {
+    // console.log(await getUser());
+    this.handleUser();
+  }
+
+  handleUser = async () => {
+    this.setState({ loading: true });
+    const user = await getUser();
+    this.setState({ loading: false,
+      user: {
+        name: 'Gustavo Rossin2313',
+        email: 'gustavo.rossin@hotmail.com',
+        image: 'url-to-image',
+        description: 'oi, eu sou o goku',
+      } });
+  };
+
   render() {
+    const { user, loading } = this.state;
     return (
-      <div data-testid="page-profile">
+      <>
         <Header />
-        Profile
-      </div>
+        { loading && <Loading /> }
+        <div data-testid="page-profile">
+          <br />
+          {user.name}
+          <br />
+          {user.email}
+          <br />
+          <img src={ user.image } data-testid="profile-image" alt={ user.name } />
+          <br />
+          {user.description}
+          <br />
+          <Link to="/profile/edit">Editar perfil</Link>
+        </div>
+      </>
     );
   }
 }
